f4349fdf6a3d2810ea85720de256aac2e42eedb5
Chris Scheele <scheele.cjs@gmail.com>
2017-04-26 19:40:04 -0500

    Better version

--- app.js
+++ app.js
@@ -3,9 +3,6 @@ var app = require("express")();
 var bodyParser = require('body-parser');
 var pg = require("pg");
 var pgp = require('pg-promise')();
-var cors = require('cors');
-
-app.use(cors({origin: 'http://localhost:8000'}));
 
 function createDBConnection(){
 	//returns a database connection
@@ -23,13 +20,56 @@ function createDBConnection(){
 //create a connection instance to use in any query
 db = createDBConnection()
 
+app.get('/delay1', function (req, res) { 
+	//get query parameters
+	var year = req.query.year
+	var month = req.query.month
+	var dayofweek = req.query.dayofweek
+	var origin = req.query.origin
+	var dest = req.query.dest
+
+	//Query
+	sql = "SELECT * FROM delays2 WHERE year = ${year} AND month = ${month} AND dayofweek = ${dayofweek} AND origincode = ${origin} AND destcode = ${dest}"
+    
+    //Define variables
+    vars = {
+    	year: year,
+    	month: month,
+    	dayofweek: dayofweek,
+    	origin: origin,
+    	dest: dest
+    }
+
+	//execute the query
+	//expect any number of rows to be returned [0, Infinity)
+	db.any(sql, vars)
+	.then(function(data){
+		//on success
+		var ts = new Date().toJSON()
+		var resOut = {
+			"success" : true,
+			"timestamp" : ts,
+			data: data
+		}
+	res.json(resOut) //finish request by sending data back to the user
+	})
+	.catch(function(err){
+		//on error
+		var ts = new Date().toJSON()
+		var resOut = {
+			"success" : false,
+			"timestamp" : ts,
+			data: []
+		}
+		res.json(resOut) //send error notifcation back to the user
+	})
+})
+
 function createAirportList(data,type){
 	var out = []
 	if (type == 1){//Percent delayed
 		for (var i=0; i < data.length; i++) {//Loop through query results
 			airport = {"name":data[i].origincode,
-				"lat":data[i].originlat,
-				"lng":data[i].originlng,
 				"stats":{
 					"ontime": data[i].perontime,
 					"cancelled": data[i].percancelled,
@@ -48,55 +88,6 @@ function createAirportList(data,type){
 	}else{
 		for (var i=0; i < data.length; i++) {//Loop through query results
 			airport = {"name":data[i].origincode,
-				"lat":data[i].originlat,
-				"lng":data[i].originlng,
-				"stats":{
-					"delayed": data[i].avgdelayed,
-					"carrierd": data[i].avgcarrierd,
-					"weatherd": data[i].avgweatherd,
-					"nasd": data[i].avgnasd,
-					"securityd": data[i].avgsecurityd,
-					"lateaircraftd": data[i].avglateaircraftd
-				},
-				"airline":[]
-			}
-			out.push(airport)
-		}
-	}
-	return out
-}
-
-function createAirportListRoutes(data,type){ //Only difference is the changing of lat and lng
-	var out = []
-	if (type == 1){//Percent delayed
-		for (var i=0; i < data.length; i++) {//Loop through query results
-			airport = {"name":data[i].origincode,
-				"originlat":data[i].originlat,
-				"originlng":data[i].originlng,
-				"destlat":data[i].destlat,
-				"desetlng":data[i].destlng,
-				"stats":{
-					"ontime": data[i].perontime,
-					"cancelled": data[i].percancelled,
-					"diverted": data[i].perdiverted,
-					"delayed": data[i].perdelayed,
-					"carrierd": data[i].percarrierd,
-					"weatherd": data[i].perweatherd,
-					"nasd": data[i].pernasd,
-					"securityd": data[i].persecurityd,
-					"lateaircraftd": data[i].perlateaircraftd
-				},
-				"airline":[]
-			}
-			out.push(airport)
-		}
-	}else{
-		for (var i=0; i < data.length; i++) {//Loop through query results
-			airport = {"name":data[i].origincode,
-				"originlat":data[i].originlat,
-				"originlng":data[i].originlng,
-				"destlat":data[i].destlat,
-				"desetlng":data[i].destlng,
 				"stats":{
 					"delayed": data[i].avgdelayed,
 					"carrierd": data[i].avgcarrierd,
@@ -160,7 +151,6 @@ function createAirlineList(data,out,type){
 	return out
 }
 
-//Get airport data
 app.get('/airports', function (req, res) { 
 	//get query parameters
 	var type = req.query.type //1=percent delayed, 0=avg delay
@@ -174,10 +164,10 @@ app.get('/airports', function (req, res) {
 
 	//Write query based on stats type
 	if (type == 1){
-		sql1 = "SELECT COALESCE(round((SUM(numontime)::float/NULLIF(SUM(numflights),0))*100),0) AS perontime,COALESCE(round((SUM(numcancelled)::float/NULLIF(SUM(numflights),0))*100),0) AS percancelled,COALESCE(round((SUM(numdiverted)::float/NULLIF(SUM(numflights),0))*100),0) AS perdiverted,COALESCE(round((SUM(numdelayed)::float/NULLIF(SUM(numflights),0))*100),0) AS perdelayed,COALESCE(round((SUM(numcarrierd)::float/NULLIF(SUM(numflights),0))*100),0) AS percarrierd,COALESCE(round((SUM(numweatherd)::float/NULLIF(SUM(numflights),0))*100),0) AS numweatherd,COALESCE(round((SUM(numnasd)::float/NULLIF(SUM(numflights),0))*100),0) AS pernasd,COALESCE(round((SUM(numsecurityd)::float/NULLIF(SUM(numflights),0))*100),0) AS persecurityd,COALESCE(round((SUM(numlateaircraftd)::float/NULLIF(SUM(numflights),0))*100),0) AS perlateaircraftd,origincode,originlat,originlng FROM delays2 WHERE (year BETWEEN $1 AND $2) AND (month BETWEEN $3 AND $4) AND (dayofweek BETWEEN $5 AND $6) AND airlineid IN ($7:csv) GROUP BY origincode,originlat,originlng ORDER BY origincode"
+		sql1 = "SELECT COALESCE(round((SUM(numontime)::float/NULLIF(SUM(numflights),0))*100),0) AS perontime,COALESCE(round((SUM(numcancelled)::float/NULLIF(SUM(numflights),0))*100),0) AS percancelled,COALESCE(round((SUM(numdiverted)::float/NULLIF(SUM(numflights),0))*100),0) AS perdiverted,COALESCE(round((SUM(numdelayed)::float/NULLIF(SUM(numflights),0))*100),0) AS perdelayed,COALESCE(round((SUM(numcarrierd)::float/NULLIF(SUM(numflights),0))*100),0) AS percarrierd,COALESCE(round((SUM(numweatherd)::float/NULLIF(SUM(numflights),0))*100),0) AS numweatherd,COALESCE(round((SUM(numnasd)::float/NULLIF(SUM(numflights),0))*100),0) AS pernasd,COALESCE(round((SUM(numsecurityd)::float/NULLIF(SUM(numflights),0))*100),0) AS persecurityd,COALESCE(round((SUM(numlateaircraftd)::float/NULLIF(SUM(numflights),0))*100),0) AS perlateaircraftd,origincode FROM delays2 WHERE (year BETWEEN $1 AND $2) AND (month BETWEEN $3 AND $4) AND (dayofweek BETWEEN $5 AND $6) AND airlineid IN ($7:csv) GROUP BY origincode ORDER BY origincode"
 		sql2 = "SELECT COALESCE(round((SUM(numontime)::float/NULLIF(SUM(numflights),0))*100),0) AS perontime,COALESCE(round((SUM(numcancelled)::float/NULLIF(SUM(numflights),0))*100),0) AS percancelled,COALESCE(round((SUM(numdiverted)::float/NULLIF(SUM(numflights),0))*100),0) AS perdiverted,COALESCE(round((SUM(numdelayed)::float/NULLIF(SUM(numflights),0))*100),0) AS perdelayed,COALESCE(round((SUM(numcarrierd)::float/NULLIF(SUM(numflights),0))*100),0) AS percarrierd,COALESCE(round((SUM(numweatherd)::float/NULLIF(SUM(numflights),0))*100),0) AS perweatherd,COALESCE(round((SUM(numnasd)::float/NULLIF(SUM(numflights),0))*100),0) AS pernasd,COALESCE(round((SUM(numsecurityd)::float/NULLIF(SUM(numflights),0))*100),0) AS persecurityd,COALESCE(round((SUM(numlateaircraftd)::float/NULLIF(SUM(numflights),0))*100),0) AS perlateaircraftd,origincode,airlinename FROM delays2 WHERE (year BETWEEN $1 AND $2) AND (month BETWEEN $3 AND $4) AND (dayofweek BETWEEN $5 AND $6) AND airlineid IN ($7:csv) GROUP BY airlinename,origincode ORDER BY origincode,airlinename"
 	}else {
-		sql1 = "SELECT COALESCE(round(SUM(numdelayed*avgdelay)::float/NULLIF(SUM(numdelayed),0)),0) AS avgDelayed,COALESCE(round(SUM(numcarrierd*carrierd)::float/NULLIF(SUM(numcarrierd),0)),0) AS avgcarrierd,COALESCE(round(SUM(numweatherd*weatherd)::float/NULLIF(SUM(numweatherd),0)),0) AS avgweatherd,COALESCE(round(SUM(numnasd*nasd)::float/NULLIF(SUM(numnasd),0)),0) AS avgnasd,COALESCE(round(SUM(numsecurityd*securityd)::float/NULLIF(SUM(numsecurityd),0)),0) AS avgsecurityd,COALESCE(round(SUM(numlateaircraftd*lateaircraftd)::float/NULLIF(SUM(numlateaircraftd),0)),0) AS avglateaircraftd,origincode,originlat,originlng FROM delays2 WHERE (year BETWEEN $1 AND $2) AND (month BETWEEN $3 AND $4) AND (dayofweek BETWEEN $5 AND $6) AND airlineid IN ($7:csv) GROUP BY origincode,originlat,originlng ORDER BY origincode"
+		sql1 = "SELECT COALESCE(round(SUM(numdelayed*avgdelay)::float/NULLIF(SUM(numdelayed),0)),0) AS avgDelayed,COALESCE(round(SUM(numcarrierd*carrierd)::float/NULLIF(SUM(numcarrierd),0)),0) AS avgcarrierd,COALESCE(round(SUM(numweatherd*weatherd)::float/NULLIF(SUM(numweatherd),0)),0) AS avgweatherd,COALESCE(round(SUM(numnasd*nasd)::float/NULLIF(SUM(numnasd),0)),0) AS avgnasd,COALESCE(round(SUM(numsecurityd*securityd)::float/NULLIF(SUM(numsecurityd),0)),0) AS avgsecurityd,COALESCE(round(SUM(numlateaircraftd*lateaircraftd)::float/NULLIF(SUM(numlateaircraftd),0)),0) AS avglateaircraftd,origincode FROM delays2 WHERE (year BETWEEN $1 AND $2) AND (month BETWEEN $3 AND $4) AND (dayofweek BETWEEN $5 AND $6) AND airlineid IN ($7:csv) GROUP BY origincode ORDER BY origincode"
 		sql2 = "SELECT COALESCE(round(SUM(numdelayed*avgdelay)::float/NULLIF(SUM(numdelayed),0)),0) AS avgDelayed,COALESCE(round(SUM(numcarrierd*carrierd)::float/NULLIF(SUM(numcarrierd),0)),0) AS avgcarrierd,COALESCE(round(SUM(numweatherd*weatherd)::float/NULLIF(SUM(numweatherd),0)),0) AS avgweatherd,COALESCE(round(SUM(numnasd*nasd)::float/NULLIF(SUM(numnasd),0)),0) AS avgnasd,COALESCE(round(SUM(numsecurityd*securityd)::float/NULLIF(SUM(numsecurityd),0)),0) AS avgsecurityd,COALESCE(round(SUM(numlateaircraftd*lateaircraftd)::float/NULLIF(SUM(numlateaircraftd),0)),0) AS avglateaircraftd,origincode,airlinename FROM delays2 WHERE (year BETWEEN $1 AND $2) AND (month BETWEEN $3 AND $4) AND (dayofweek BETWEEN $5 AND $6) AND airlineid IN ($7:csv) GROUP BY airlinename,origincode ORDER BY origincode,airlinename"
 	}
 
@@ -194,62 +184,6 @@ app.get('/airports', function (req, res) {
 		//on success
 		//Build airport list
 		var out = createAirportList(data[0],type)
-		//Build airline list for airport list
-		out = createAirlineList(data[1],out,type)
-		var resOut = {
-			"type":type,
-			"success" : true,
-			data: out
-		}
-	res.json(resOut) //finish request by sending data back to the user
-	})
-    .catch(function(err){
-		//on error
-		console.log(err)
-		var resOut = {
-			"type":type,
-			"success" : false,
-			data: []
-		}
-		res.json(resOut) //send error notifcation back to the user
-    });
-})
-
-//Get route data
-app.get('/routes', function (req, res) { 
-	//get query parameters
-	var type = req.query.type //1=percent delayed, 0=avg delay
-	var fyr = req.query.fyr
-	var lyr = req.query.lyr
-	var fmth = req.query.fmth
-	var lmth = req.query.lmth
-	var fdow = req.query.fdow
-	var ldow = req.query.ldow
-	var airline = req.query.airlines.split(',')
-	var dest = req.query.dest
-
-	//Write query based on stats type
-	if (type == 1){
-		sql1 = "SELECT COALESCE(round((SUM(numontime)::float/NULLIF(SUM(numflights),0))*100),0) AS perontime,COALESCE(round((SUM(numcancelled)::float/NULLIF(SUM(numflights),0))*100),0) AS percancelled,COALESCE(round((SUM(numdiverted)::float/NULLIF(SUM(numflights),0))*100),0) AS perdiverted,COALESCE(round((SUM(numdelayed)::float/NULLIF(SUM(numflights),0))*100),0) AS perdelayed,COALESCE(round((SUM(numcarrierd)::float/NULLIF(SUM(numflights),0))*100),0) AS percarrierd,COALESCE(round((SUM(numweatherd)::float/NULLIF(SUM(numflights),0))*100),0) AS perweatherd,COALESCE(round((SUM(numnasd)::float/NULLIF(SUM(numflights),0))*100),0) AS pernasd,COALESCE(round((SUM(numsecurityd)::float/NULLIF(SUM(numflights),0))*100),0) AS persecurityd,COALESCE(round((SUM(numlateaircraftd)::float/NULLIF(SUM(numflights),0))*100),0) AS perlateaircraftd,origincode,originlat,originlng,destlat,destlng FROM delays2 WHERE (year BETWEEN $1 AND $2) AND (month BETWEEN $3 AND $4) AND (dayofweek BETWEEN $5 AND $6) AND airlineid IN ($7:csv) AND destcode = $8 GROUP BY origincode,originlat,originlng,destlat,destlng ORDER BY origincode"
-		sql2 = "SELECT COALESCE(round((SUM(numontime)::float/NULLIF(SUM(numflights),0))*100),0) AS perontime,COALESCE(round((SUM(numcancelled)::float/NULLIF(SUM(numflights),0))*100),0) AS percancelled,COALESCE(round((SUM(numdiverted)::float/NULLIF(SUM(numflights),0))*100),0) AS perdiverted,COALESCE(round((SUM(numdelayed)::float/NULLIF(SUM(numflights),0))*100),0) AS perdelayed,COALESCE(round((SUM(numcarrierd)::float/NULLIF(SUM(numflights),0))*100),0) AS percarrierd,COALESCE(round((SUM(numweatherd)::float/NULLIF(SUM(numflights),0))*100),0) AS perweatherd,COALESCE(round((SUM(numnasd)::float/NULLIF(SUM(numflights),0))*100),0) AS pernasd,COALESCE(round((SUM(numsecurityd)::float/NULLIF(SUM(numflights),0))*100),0) AS persecurityd,COALESCE(round((SUM(numlateaircraftd)::float/NULLIF(SUM(numflights),0))*100),0) AS perlateaircraftd,origincode,airlinename FROM delays2 WHERE (year BETWEEN $1 AND $2) AND (month BETWEEN $3 AND $4) AND (dayofweek BETWEEN $5 AND $6) AND airlineid IN ($7:csv) AND destcode = $8 GROUP BY airlinename,origincode ORDER BY origincode,airlinename"
-	}else {
-		sql1 = "SELECT COALESCE(round(SUM(numdelayed*avgdelay)::float/NULLIF(SUM(numdelayed),0)),0) AS avgDelayed,COALESCE(round(SUM(numcarrierd*carrierd)::float/NULLIF(SUM(numcarrierd),0)),0) AS avgcarrierd,COALESCE(round(SUM(numweatherd*weatherd)::float/NULLIF(SUM(numweatherd),0)),0) AS avgweatherd,COALESCE(round(SUM(numnasd*nasd)::float/NULLIF(SUM(numnasd),0)),0) AS avgnasd,COALESCE(round(SUM(numsecurityd*securityd)::float/NULLIF(SUM(numsecurityd),0)),0) AS avgsecurityd,COALESCE(round(SUM(numlateaircraftd*lateaircraftd)::float/NULLIF(SUM(numlateaircraftd),0)),0) AS avglateaircraftd,origincode,originlat,originlng,destlat,destlng FROM delays2 WHERE (year BETWEEN $1 AND $2) AND (month BETWEEN $3 AND $4) AND (dayofweek BETWEEN $5 AND $6) AND airlineid IN ($7:csv) AND destcode = $8 GROUP BY origincode,originlat,originlng,destlat,destlng ORDER BY origincode"
-		sql2 = "SELECT COALESCE(round(SUM(numdelayed*avgdelay)::float/NULLIF(SUM(numdelayed),0)),0) AS avgDelayed,COALESCE(round(SUM(numcarrierd*carrierd)::float/NULLIF(SUM(numcarrierd),0)),0) AS avgcarrierd,COALESCE(round(SUM(numweatherd*weatherd)::float/NULLIF(SUM(numweatherd),0)),0) AS avgweatherd,COALESCE(round(SUM(numnasd*nasd)::float/NULLIF(SUM(numnasd),0)),0) AS avgnasd,COALESCE(round(SUM(numsecurityd*securityd)::float/NULLIF(SUM(numsecurityd),0)),0) AS avgsecurityd,COALESCE(round(SUM(numlateaircraftd*lateaircraftd)::float/NULLIF(SUM(numlateaircraftd),0)),0) AS avglateaircraftd,origincode,airlinename FROM delays2 WHERE (year BETWEEN $1 AND $2) AND (month BETWEEN $3 AND $4) AND (dayofweek BETWEEN $5 AND $6) AND airlineid IN ($7:csv) AND destcode = $8 GROUP BY airlinename,origincode ORDER BY origincode,airlinename"
-	}
-
-    //Define variables
-    vars = [fyr,lyr,fmth,lmth,fdow,ldow,airline,dest]
-	//execute the query
-	db.task(t => {
-		return t.batch([
-			t.any(sql1, vars),
-			t.any(sql2, vars)
-		]);
-	})
-	.then(function(data){
-		//on success
-		//Build airport list
-		var out = createAirportListRoutes(data[0],type)
 		//Build airline list for airport list
 		out = createAirlineList(data[1],out,type)
 		var resOut = {
--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 //wrap everything in a self-executing anonymous function to move to local scope
 (function(){
-var airports,map;
+
+var map,projection;
+
+var airportsURL = 'http://localhost:8081/airports'
+var routesURL = 'http://localhost:8081/routes'
+
 
 //begin script when window loads
 window.onload = setMap();
@@ -9,25 +14,24 @@ window.onload = setMap();
 function setMap(){
 
     //map frame dimensions
-    var width = $("#mapWindow").innerWidth(),
-        height = 460;
+    var width = $("#mapDiv").innerWidth(),
+        height =$("#mapDiv").innerHeight();
 
     //create new svg container for the map
-    map = d3.select("#mapWindow")
+    map = d3.select("#mapDiv")
+	
         .append("svg")
         .attr("class", "map")
         .attr("width", width)
         .attr("height", height);
-
     
-
     //create Albers equal area conic projection
-    var projection = d3.geoAlbers()
+    projection = d3.geoAlbers()
         .center([-0.8, 39.96])
         .rotate([93.73, 0.91, 0])
         .parallels([35.68, 45.50])
-        .scale(880)
-        .translate([width / 2, height / 2]);
+        .scale(1750)
+        .translate([width/2, height/2]);
 
     //Azimuthal
     // var projection = d3.geo.azimuthal()
@@ -41,26 +45,18 @@ function setMap(){
 
     //use d3.queue to parallelize asynchronous data loading
     d3.queue()
-        .defer(d3.json, "data/airports.json") //load airpots
         .defer(d3.json, "data/states.topojson") //load background states
         .await(callback);
-
-    function callback(error,airports,states){
+	
+    function callback(error,states){
         //Translate the topojson
         var states_topo = topojson.feature(states, states.objects.collection);
 
-        for (i = 0; i < airports.features.length; i++) {
-            var location = [+airports.features[i].properties.lng, +airports.features[i].properties.lat]
-            var position = projection(location)
-            airports.features[i].properties["position"] = position
-        }
         //Generate map
         setStateOverlay(states_topo, map, path);
-        //setAirports(airports,map);
+        //getFirstAirportDelays(map);
 
-        
-            
-     
+ 
     };
 };
 
@@ -72,94 +68,117 @@ function setStateOverlay(states_topo, map, path){
         .attr("d", path);
 };
 
-//add airports to map
-function setAirports(airports, map){
-    var circles = map.append("svg")
-        .attr("id", "circles");
-    circles.selectAll("circles")
-        .data(airports.features)
-        .enter()
-        .append("circle")
-            .attr('cx', function(d) {return d.properties.position[0]})
-            .attr('cy', function(d) { return d.properties.position[1]})
-            .attr("r", function(d) { return d.properties.rank/8; })
-            .attr("id",function(d) { return d.properties.iata});
-    //         .on("mouseover", function(d){
-    //             highlightAirport(d.properties);
-    //         })
-    //         .on("mouseout", function(d){
-    //             dehighlightAirport(d.properties)
-    //         });
-    // var desc = circles.append("desc")
-    //         .text('{"stroke": "white", "stroke-width": "0","stroke-opacity": "0"}'); 
-    getAirportDelays()   
-};
+// function getFirstAirportDelays(map){
+//     $.ajax({
+//         url: airportsURL,
+//         data: {
+//             type: 1,
+//             fyr: 2014,
+//             lyr: 2014,
+//             fmth: 1,
+//             lmth: 12,
+//             fdow: 1,
+//             ldow: 1,
+//             airlines: eval([19393,19930]).join(",")
+//         },
+//         error: function() {
+//             console.log("error");
+//         },
+//         dataType: 'json',
+//         success: function(data) {
+//         	console.log(data)
+//             setAirportDelays(data.data,map);
+//         },
+//         type: 'GET'
+//     });
+// }
 
-function getAirportDelays(){
-    $.ajax({
-        url: 'http://localhost:8081/airports',
+// function setAirportDelays(airports,map){
+// 	for (i = 0; i < airports.length; i++) {
+//             var location = [+airports[i].lng, +airports[i].lat]
+//             var position = projection(location)
+//             airports[i]["position"] = position
+//         }
+
+//     var circles = map.append("svg")
+//         .attr("id", "circles");
+//     circles.selectAll("circles")
+//         .data(airports)
+//         .enter()
+//         .append("circle")
+//             .attr('cx', function(d) {return d.position[0]})
+//             .attr('cy', function(d) { return d.position[1]})
+//             .attr("r", function(d) { return d.stats.delayed/3;})
+//             .attr("id",function(d) { return d.code});
+// }
+
+var changeTimer = false;
+$("input[name=proportional_symbol],#yearInput,#monthInput,#dayInput,input[name=delay],input[name=airline],input[name=checkBtn]" ).on("change",function(){
+    if(changeTimer !== false) clearTimeout(changeTimer);
+    changeTimer = setTimeout(function(){
+        var type = $('input[name=proportional_symbol]:checked').val()
+        var fyr = $('#yearInput').val().split(",")[0]
+        var lyr = $('#yearInput').val().split(",")[1]
+        var fmth = $('#monthInput').val().split(",")[0]
+        var lmth = $('#monthInput').val().split(",")[1]
+        var fdow = $('#dayInput').val().split(",")[0]
+        var ldow = $('#dayInput').val().split(",")[1]
+        var delay = $('input[name=delay]:checked').val()
+        var airline = $("input[name=airline]:checked").map(function() {
+			return parseInt(this.value);
+		}).get();
+
+	//Do ajax call	
+	$.ajax({
+        url: airportsURL,
         data: {
-            type: 1,
-            fyr: 2014,
-            lyr: 2014,
-            fmth: 1,
-            lmth: 12,
-            fdow: 1,
-            ldow: 1,
-            airlines: eval([19393,19930]).join(",")
+            type: type,
+            fyr: fyr,
+            lyr: lyr,
+            fmth: fmth,
+            lmth: lmth,
+            fdow: fdow,
+            ldow: ldow,
+            airlines: eval(airline).join(",")
         },
         error: function() {
             console.log("error");
         },
         dataType: 'json',
         success: function(data) {
-            updateAirportDelays(data);
+            updateAirportDelays(data.data,map,delay);
         },
         type: 'GET'
     });
-}
 
-function updateAirportDelays(response){
-    var circles = d3.selectAll("#circles")
-        .data(response)
+            changeTimer = false;
+        },150);
+});
+
+function updateAirportDelays(airports,map,delayType){
+	for (i = 0; i < airports.length; i++) {
+            var location = [+airports[i].lng, +airports[i].lat]
+            var position = projection(location)
+            airports[i]["position"] = position
+        }
+
+    map.selectAll("circles").exit().remove();
+    var circles = map.append("svg")
+    	.attr("id", "circles");
+
+	circles.selectAll("circles")
+        .data(airports)
         .enter()
         .append("circle")
-            .attr('cx', function(d) {return d.properties.position[0]})
-            .attr('cy', function(d) { return d.properties.position[1]})
-            .attr("r", function(d) { return d.properties.rank/8; })
-            .attr("id",function(d) { return d.properties.iata});
-
-    for (i = 0; i < response.data.length; i++) {
-        var circle = circles.select("#"+ response.data[i].name)
-            .data(response)
-            .attr("r", function(d) { return d.data[i].stats.delayed/8;});
-    }
+	        .attr('cx', function(d) {return d.position[0]})
+	        .attr('cy', function(d) { return d.position[1]})
+	        .attr("r", function(d) { return d.stats.delayed/3;})
+	        .style("fill",function() {
+    return "hsl(" + Math.random() * 360 + ",100%,50%)";
+    });
 }
 
-    //     var data = [airports,response]
-    //     var circles d3.selectAll("circles")
-    //         .data(data)
-    //         .enter()
-    //         .append("circle")
-    //             .attr('cx', function(d) {return d.properties.position[0]})
-    //             .attr('cy', function(d) { return d.properties.position[1]})
-    //             .attr("r", function(d) { return d.properties.rank/8; })
-    //             .sort(function(a, b) { return b.properties.rank - a.properties.rank; })
-    //             .attr("id",function(d) { return d.properties.iata});
 
-
-    // var circles = d3.selectAll("#circles").each(
-    //     function(i){
-    //         var circle
-    //     }
-
-
-
-    //     )
-    //     .attr("r", function(response){
-
-
-    //     })
 
 function highlightAirport(props){
     //change stroke
@@ -196,6 +215,151 @@ function dehighlightAirport(props){
     };
 };
 
+	//navigation bar controls
+	$(".nav-item").hover(function(){
+	$(this).toggleClass('navbar-hovered')
+	}, function(){
+	$(this).toggleClass('navbar-hovered')
+	})
 
-
+	$(".nav-item").click(function(){
+		$(".nav-item").removeClass("active")
+		$(this).addClass("active")
+		
+		//figure out what to display
+		$(".navbar-panel").css({'display': "none"})
+		_thisData = $(this).data('panel')
+		if (_thisData == "search"){
+			$("#search-panel").slideToggle()
+		}else if (_thisData == "proportion"){
+			$("#proportion-panel").slideToggle()
+		}else if (_thisData == "time"){
+			$("#time-panel").slideToggle()
+		}else if (_thisData == "delay"){
+			$("#delay-panel").slideToggle()
+		}else if (_thisData == "airline"){
+			$("#airline-panel").slideToggle()
+		}else{
+			return
+		}
+		/*
+		$(".navbar-panel").css({'display': "none"})
+		_thisData = $(this).data('panel')
+		if (_thisData == 'intro'){
+			$("#intro-panel").slideToggle()
+		}else if (_thisData == "search"){
+			$("#search-panel").slideToggle()
+		}else if (_thisData == "proportion"){
+			$("#proportion-panel").slideToggle()
+		}else if (_thisData == "time"){
+			$("#time-panel").slideToggle()
+		}else if (_thisData == "delay"){
+			$("#delay-panel").slideToggle()
+		}else if (_thisData == "airline"){
+			$("#airline-panel").slideToggle()
+		}else{
+			return
+		}
+		*/
+	})
+	
+	//close filter panel when user clicks outside the panel
+	$("#mapDiv").click(function(){
+		$(".control-panel").css({'display': "none"})
+		$(".nav-item").removeClass("active")
+	})
+	
+	//dehighlight the return icon once mouse is moved out
+	$("#navbar-return").mouseout(function(){
+		$(".nav-item").removeClass("active")
+	})
+	
+	//reset the map back to default
+	$(document).ready(function(){
+		$("#navbar-return").on("click",function(){
+			$(".control-panel").trigger("reset");
+		})
+	})
+	
+	//range sliders
+	$(".range-slider1").jRange({
+		from:2014,
+		to:2016,
+		step:1,
+		scale:[2014,2015,2016],
+		width:230,
+		showLabels:false,
+		isRange:true,
+		snap:true
+	})
+	
+	$(".range-slider2").jRange({
+		from:1,
+		to:12,
+		step:1,
+		scale:['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'],
+		width:230,
+		showLabels:false,
+		isRange:true,
+		snap:true
+	})
+	
+	$(".range-slider3").jRange({
+		from:1,
+		to:7,
+		step:1,
+		scale:['Mon','Tue','Wed','Thu','Fri','Sat','Sun'],
+		width:230,
+		showLabels:false,
+		isRange:true,
+		snap:true
+	})
+	
+	//airline checkboxes
+	$(document).ready(function() {
+		var checkBoxes = $("input[name=airline]");
+		checkBoxes.prop("checked", true);
+	$(".check").click(function() {
+		var checkBoxes = $("input[name=airline]");
+		checkBoxes.prop("checked", !checkBoxes.prop("checked"));
+		if (checkBoxes.prop("checked")){
+			$(this).val("Uncheck All")}
+		else{
+			$(this).val("Check All")}
+		});   
+	});
+	
+	//intro window
+	d3.select(".container2")
+	.append("div")
+	.attr("class","grayOut")
+			
+	d3.select("body")
+		.append("div").attr("class","OverviewBox")
+		.html("<span class='OverviewBoxTitle'><p>Welcome to U.S. Delay Flight Tracker</p></span><span class='OverviewBoxContent'><p>This interactive map is for exploring the temporal and spatial trends of delay domestic flights within the U.S. from 2014 to 2016. We believe that users will make better and smarter itinerary decisions by comparing the historic differences in delay frequencies between airlines.<br> To detect more insights, you can use the filters on the left-hand side to investigate information such as the percentage of delay flights per airport, average delay time per airport, delay patterns across time and airlines, types of flight delay, etc.<br> If you want to get a more intuitive guide on how to use this map, please watch the tutorial.</p></span>")
+		.append("button").attr("class","OverviewButton")
+		.text("Click Here to Enter the Map")
+		.on("click",function(){
+			d3.select(".OverviewBox").remove()
+			d3.select(".grayOut").remove()
+	})
+	
+	//fuzzy search
+	// var options = {
+	// 	shouldSort: true,
+	// 	includeMatches: true,
+	// 	threshold: 0.5,
+	// 	location: 0,
+	// 	distance: 100,
+	// 	maxPatternLength: 24,
+	// 	minMatchCharLength: 3,
+	// 	keys:[
+	// 	"name",
+	// 	"city",
+	// 	"iata"
+	// 	]
+	// };
+	// var fuse = new Fuse(airports, options); // "list" is the item array
+	// var result = fuse.search("");
+	
 })();
